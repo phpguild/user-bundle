faa29abfe077cb86251268968cbc169808ef7b64
Jonathan SAHM <pro@jsahm.fr>
2021-02-19 21:04:26 +0100

    Add phpguild/doctrine-extra-bundle dependency

--- src/Model/UserInterface.php
+++ src/Model/UserInterface.php
@@ -4,9 +4,11 @@ declare(strict_types=1);
 
 namespace PhpGuild\UserBundle\Model;
 
+use PhpGuild\DoctrineExtraBundle\Model\IdInterface;
+
 /**
  * Interface UserInterface
  */
-interface UserInterface extends \Symfony\Component\Security\Core\User\UserInterface
+interface UserInterface extends IdInterface, \Symfony\Component\Security\Core\User\UserInterface
 {
 }
